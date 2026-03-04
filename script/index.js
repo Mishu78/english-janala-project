3f14e7b8a8436281f955878dda72f0aa70f78bac
Mishu78 <meherun2002@gmail.com>
2026-03-04 13:58:14 +0600

    icons are added

--- script/index.js
+++ script/index.js
@@ -7,17 +7,36 @@ const loadLessons=()=>{
     .then((json)=>displayLessons(json.data));
 }
 
+const loadLevelWord=(id)=>{
+    console.log(id);
+    const url=`https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>displayLevelWord(data.data));
+} 
+const displayLevelWord=(words)=>{
+    const wordContainer=document.getElementById('word-container')
+    //wordContainer.innerHTML='';
+    for(const word of words){
+    console.log(word);
+    const card=document.createElement('div');
+    card.innerHTML=`
+    <p>Cat</p>
+    `
+    wordContainer.append(card);
+    }
+}
 const displayLessons=(lessons)=>{
  //1.get the container & empty
  const levelContainer=document.getElementById('level-container');
- levelContainer.innerText='';
+ levelContainer.innerHTML='';
  //2.get into every lesson
  for(let lesson of lessons){
-    console.log(lesson)
+    //console.log(lesson)
 //3.create element
 const btnDiv=document.createElement('div');
 btnDiv.innerHTML=`
-<button class="btn btn-outline btn-primary">
+<button onClick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
 <i class="fa-solid fa-book-open-reader"></i> Lesson-${lesson.level_no}</button>
 `
  //4.append into container
